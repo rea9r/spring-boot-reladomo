f9b9cfbc97bfca406bea9ac129d454b04e86296b
amtkxa <amtkxa@gmail.com>
2018-11-24 22:43:29 +0900

    first commit

---
+++ Java/spring-boot-reladomo/src/main/java/com/amtkxa/springbootreladomo/SpringBootReladomoApplication.java
@@ -0,0 +1,12 @@
+package com.amtkxa.springbootreladomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReladomoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootReladomoApplication.class, args);
+  }
+}
---
+++ Java/spring-boot-reladomo/src/test/java/com/amtkxa/springbootreladomo/SpringBootReladomoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amtkxa.springbootreladomo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootReladomoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
