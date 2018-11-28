7189a4b90928df92373cf27239fbd929f7fc8a3e
amtkxa <scarlet0211@outlook.jp>
2018-11-29 00:36:27 +0900

    @RestController を利用することとして、@ResponseBody を削除

--- src/main/java/com/amtkxa/springbootreladomo/controller/CustomerController.java
+++ src/main/java/com/amtkxa/springbootreladomo/controller/CustomerController.java
@@ -5,7 +5,10 @@ import com.amtkxa.springbootreladomo.service.impl.CustomerServiceImpl;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,7 +21,6 @@ public class CustomerController {
     private final CustomerServiceImpl customerServiceImpl;
 
     @RequestMapping(method = RequestMethod.GET, value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
     public List<? extends CustomerView> getCustomerAll() {
         return customerServiceImpl.findAll();
     }
