bb673184e385133e9266a3c07e3e78fa41f4b64f
amtkxa <scarlet0211@outlook.jp>
2018-12-01 00:02:39 +0900

    Presenter 追加

---
+++ src/main/java/com/amtkxa/springbootreladomo/Interface/presenter/CustomerPresenter.java
@@ -0,0 +1,19 @@
+package com.amtkxa.springbootreladomo.Interface.presenter;
+
+import com.amtkxa.springbootreladomo.domain.model.CustomerList;
+import com.amtkxa.springbootreladomo.usecase.view.CustomerView;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomerPresenter {
+    public List<? extends CustomerView> response(CustomerList customerList) {
+        return customerList
+                .stream()
+                .filter(c -> c != null)
+                .map(c -> CustomerView.fromCustomer(c))
+                .collect(Collectors.toList());
+    }
+}
