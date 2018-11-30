49d591e13c56be7b96710d8456e97e4557b27810
amtkxa <scarlet0211@outlook.jp>
2018-12-01 00:04:00 +0900

    Controller クラスで使用しているアノテーションを変更

--- src/main/java/com/amtkxa/springbootreladomo/Interface/controller/CustomerController.java
+++ src/main/java/com/amtkxa/springbootreladomo/Interface/controller/CustomerController.java
@@ -5,28 +5,25 @@ import com.amtkxa.springbootreladomo.usecase.view.CustomerView;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/customer")
 @RequiredArgsConstructor
 public class CustomerController {
 
-    @NonNull
-    private final CustomerServiceImpl customerServiceImpl;
+  @NonNull private final CustomerServiceImpl customerServiceImpl;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<? extends CustomerView> getCustomerAll() {
-        return customerServiceImpl.findAll();
-    }
+  @GetMapping(value = "/api/customer", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<? extends CustomerView> getCustomerAll() {
+    return customerServiceImpl.findAll();
+  }
 
-    @RequestMapping(method = RequestMethod.GET, value = "{customerId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<? extends CustomerView> getCustomerById(@PathVariable("customerId") int customerId) {
-        return customerServiceImpl.findByCustomerId(customerId);
-    }
+  @GetMapping(value = "/api/customer/{customerId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<? extends CustomerView> getCustomerById(@PathVariable("customerId") int customerId) {
+    return customerServiceImpl.findByCustomerId(customerId);
+  }
 }
--- src/main/java/com/amtkxa/springbootreladomo/usecase/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/amtkxa/springbootreladomo/usecase/service/impl/CustomerServiceImpl.java
@@ -1,34 +1,31 @@
 package com.amtkxa.springbootreladomo.usecase.service.impl;
 
-import com.amtkxa.springbootreladomo.domain.model.CustomerFinder;
+import com.amtkxa.springbootreladomo.Interface.presenter.CustomerPresenter;
 import com.amtkxa.springbootreladomo.domain.model.CustomerList;
+import com.amtkxa.springbootreladomo.usecase.repository.impl.CustomerRepositoryImpl;
 import com.amtkxa.springbootreladomo.usecase.service.CustomerService;
 import com.amtkxa.springbootreladomo.usecase.view.CustomerView;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+  @NonNull private final CustomerRepositoryImpl customerRepositoryImpl;
+  @NonNull private final CustomerPresenter customerPresenter;
 
-    @Override
-    public List<? extends CustomerView> findAll() {
-        CustomerList customerList = new CustomerList(CustomerFinder.all());
-        return customerList
-                .stream()
-                .filter(c -> c != null)
-                .map(c -> CustomerView.fromCustomer(c))
-                .collect(Collectors.toList());
-    }
+  @Override
+  public List<? extends CustomerView> findAll() {
+    CustomerList customerList = customerRepositoryImpl.findAll();
+    return customerPresenter.response(customerList);
+  }
 
-    @Override
-    public List<? extends CustomerView> findByCustomerId(int customerId) {
-        CustomerList customerList = CustomerFinder.findMany(CustomerFinder.customerId().eq(customerId));
-        return customerList
-                .stream()
-                .filter(c -> c != null)
-                .map(c -> CustomerView.fromCustomer(c))
-                .collect(Collectors.toList());
-    }
+  @Override
+  public List<? extends CustomerView> findByCustomerId(int customerId) {
+    CustomerList customerList = customerRepositoryImpl.findByCustomerId(customerId);
+    return customerPresenter.response(customerList);
+  }
 }
