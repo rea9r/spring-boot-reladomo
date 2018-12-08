344d315c87d99bd0692b19b4c20729ebe238f466
amtkxa <scarlet0211@outlook.jp>
2018-12-08 13:04:12 +0900

    CustomerAccountRepositoryImpl の実装を記述

--- src/main/java/com/amtkxa/springbootreladomo/usecase/repository/impl/CustomerAccountRepositoryImpl.java
+++ src/main/java/com/amtkxa/springbootreladomo/usecase/repository/impl/CustomerAccountRepositoryImpl.java
@@ -1,8 +1,14 @@
 package com.amtkxa.springbootreladomo.usecase.repository.impl;
 
+import com.amtkxa.springbootreladomo.domain.model.CustomerAccount;
+import com.amtkxa.springbootreladomo.domain.model.CustomerAccountFinder;
 import com.amtkxa.springbootreladomo.domain.model.CustomerAccountList;
+import com.amtkxa.springbootreladomo.domain.model.CustomerFinder;
+import com.amtkxa.springbootreladomo.infrastructure.util.DateUtils;
 import com.amtkxa.springbootreladomo.usecase.repository.CustomerAccountRepository;
 import com.amtkxa.springbootreladomo.usecase.view.CustomerAccountView;
+import com.gs.fw.common.mithra.MithraManagerProvider;
+import com.gs.fw.common.mithra.finder.Operation;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,22 +16,38 @@ public class CustomerAccountRepositoryImpl implements CustomerAccountRepository 
 
   @Override
   public CustomerAccountList findAll() {
-    return null;
+    Operation ts = CustomerAccountFinder.businessDate().equalsEdgePoint();
+    return CustomerAccountFinder.findMany(ts);
   }
 
   @Override
   public CustomerAccountList findByCustomerId(int customerId) {
-    return null;
+    Operation id = CustomerAccountFinder.customerId().eq(customerId);
+    Operation ts = CustomerAccountFinder.businessDate().equalsEdgePoint();
+    return CustomerAccountFinder.findMany(id.and(ts));
   }
 
   @Override
   public CustomerAccountList create(CustomerAccountView customerAccountView) {
-    return null;
+    CustomerAccount customerAccount = new CustomerAccount(customerAccountView);
+    customerAccount.cascadeInsert();
+    return new CustomerAccountList(customerAccount);
   }
 
   @Override
   public CustomerAccountList update(CustomerAccountView customerAccountView) {
-    return null;
+    MithraManagerProvider.getMithraManager().executeTransactionalCommand((tx) -> {
+      // fetch data with businessDate
+      Operation id = CustomerFinder.customerId().eq(customerAccountView.getCustomerId());
+      Operation ts = CustomerFinder.businessDate().eq(DateUtils.parse(customerAccountView.getBusinessDate()));
+      CustomerAccount customerAccount = CustomerAccountFinder.findOne(id.and(ts));
+      // update
+      customerAccount.setAccountName(customerAccountView.getAccountName());
+      customerAccount.setAccountType(customerAccountView.getAccountType());
+      customerAccount.setBalance(customerAccountView.getBalance());
+      return null;
+    });
+    return findByCustomerId(customerAccountView.getCustomerId());
   }
 
   @Override
