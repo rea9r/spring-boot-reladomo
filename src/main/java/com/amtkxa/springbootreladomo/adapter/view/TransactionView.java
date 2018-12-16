2d2e64e105136f828c27d04da16da321473df538
amtkxa <scarlet0211@outlook.jp>
2018-12-16 12:51:00 +0900

    Transaction の transactionDate のデフォルト値を適用

--- src/main/java/com/amtkxa/springbootreladomo/adapter/view/TransactionView.java
+++ src/main/java/com/amtkxa/springbootreladomo/adapter/view/TransactionView.java
@@ -4,8 +4,10 @@ import com.amtkxa.springbootreladomo.domain.entity.TransactionType;
 import com.amtkxa.springbootreladomo.infrastructure.util.DateUtils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -13,18 +15,22 @@ import java.sql.Timestamp;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionView {
   @JsonIgnore
   @Enumerated(EnumType.STRING)
   @ApiModelProperty(value = "Transaction transactionType.")
   private TransactionType transactionType;
 
+  @Builder.Default
   @ApiModelProperty(value = "Date the change actually occurred.")
-  private Timestamp transactionDate;
+  private Timestamp transactionDate = DateUtils.getCurrentTimestamp();
 
   @ApiModelProperty(value = "Account ID.")
   private int accountId;
 
   @ApiModelProperty(value = "Customer's balance.")
   private double amount;
+
 }
