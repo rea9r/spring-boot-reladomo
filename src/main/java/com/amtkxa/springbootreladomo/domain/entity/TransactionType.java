1b3c0daba6027a73d5b6752678ce758f5b4091f1
amtkxa <scarlet0211@outlook.jp>
2018-12-16 14:27:11 +0900

    TransactionType に関するコメントを記載

--- src/main/java/com/amtkxa/springbootreladomo/domain/entity/TransactionType.java
+++ src/main/java/com/amtkxa/springbootreladomo/domain/entity/TransactionType.java
@@ -1,5 +1,12 @@
 package com.amtkxa.springbootreladomo.domain.entity;
 
+/**
+ * Identifies the type of transaction.
+ */
 public enum TransactionType {
-  WITHDRAWAL, DEPOSIT
+  // Removing funds from a account.
+  WITHDRAWAL,
+
+  // Adding funds into a registered account.
+  DEPOSIT
 }
